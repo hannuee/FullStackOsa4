0b339e450cae1145af5e1567af7598d0607e23f2
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2021-02-20 18:14:20 +0200

    Tehtävä 4.5 apufunktioita ja yksikkötestejä step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,12 +2,25 @@ const dummy = (blogs) => {
     return 1
 }
 
-const totalLikes = (blogs) => blogs.reduce((sum, blog) => {
-    if (typeof blog.likes !== 'undefined' && Number.isInteger(blog.likes)) return sum + blog.likes
-    return sum
-}, 0)
+const totalLikes = (blogs) => 
+    blogs.reduce((sum, blog) => {
+        if (typeof blog.likes !== 'undefined' && Number.isInteger(blog.likes)) return sum + blog.likes
+        return sum
+    }, 0)
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return NaN
+    
+    let max = blogs[0]
+    blogs.forEach(blog => {
+        if (blog.likes > max.likes) max = blog
+    })
+
+    return {title: max.title, author: max.author, likes: max.likes}
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
